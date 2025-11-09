aea0c0b69c5181b2ea24ebff64b6199f72f3e86d
tiagofpr <tiagofpr@outlook.com>
2025-11-09 18:55:34 -0300

    primeiro commit, criacao projeto no spring e configuracao de banco de dados

---
+++ src/main/java/br/com/compras/ApiCompraApplication.java
@@ -0,0 +1,13 @@
+package br.com.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompraApplication.class, args);
+	}
+
+}
